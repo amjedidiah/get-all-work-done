b3f049ab8c2d61911224addd1d43c8034dd4baad
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-02-09 19:31:26 +0100

    fix(app): cors issue
    fixed cors issue

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import { getWorld } from '@get-all-work-done/shared/utils';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 8008;
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({ message: `Hello ${getWorld()}` });
+});
+
+app.get('/list', (req, res) => {
+  res.send({ message: 'list' });
 });
 
 app.listen(port, host, () => {
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,18 @@
+'use client';
 import { getWorld } from '@get-all-work-done/shared/utils';
+import { useEffect, useState } from 'react';
 
 const name = getWorld();
-export default async function Index() {
+export default function Index() {
+  const [list, setList] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:8008/list')
+      .then((res) => res.json())
+      .then(setList);
+  }, []);
+
+  console.log(list);
+
   return <div className="">Hello {name}</div>;
 }
