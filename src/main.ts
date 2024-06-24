f65553a1ddd3590a277ed96946c3c22da95cb8e4
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-06-24 09:58:22 +0100

    build(app): fly redeploy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { isDev } from './constants';
 
 // App
 const app = express();
-const host = process.env.HOST ?? 'localhost';
-const port = 8080;
+const host = isDev ? 'localhost' : '0.0.0.0';
+const port = isDev ? 8080 : 3000;
 
 app.use(cors());
 app.use(express.json());
